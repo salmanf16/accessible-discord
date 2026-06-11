bc8d6fd999454acf23957ccbfe535317f29435ed
salmanf16 <salman2222222222f16@gmail.com>
2026-06-11 04:04:11 +0300

    Update minimum NVDA version to 2023.1

--- buildVars.py
+++ buildVars.py
@@ -7,6 +7,6 @@ addon_info = {
     "addon_author": "salmanf16",
     "addon_url": "https://github.com/salmanf16/accessible-discord",
     "addon_version": "1.0.0",
-    "addon_minimumNVDAVersion": "2021.1",
+    "addon_minimumNVDAVersion": "2023.1",
     "addon_lastTestedNVDAVersion": "2026.1",
 }
