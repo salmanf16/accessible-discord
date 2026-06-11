4aba3462ed4ab0f56a72c3b0b6c9a090fb19e333
salmanf16 <salman2222222222f16@gmail.com>
2026-06-11 21:11:13 +0300

    Log all permutations of stream metadata query methods

--- globalPlugins/AccessibleDiscord.plugin.js
+++ globalPlugins/AccessibleDiscord.plugin.js
@@ -245,13 +245,19 @@ module.exports = class AccessibleDiscord {
             if (ApplicationStreamingStore) {
                 let stream = ApplicationStreamingStore.getAnyStreamForUser(userId);
                 if (stream) {
-                    let meta = ApplicationStreamingStore.getStreamerActiveStreamMetadataForStream(stream);
+                    const streamKey = `${stream.streamType}:${stream.guildId}:${stream.channelId}:${stream.ownerId}`;
+                    
+                    let m1 = ApplicationStreamingStore.getStreamerActiveStreamMetadata(userId);
+                    let m2 = ApplicationStreamingStore.getStreamerActiveStreamMetadata(streamKey);
+                    let m3 = ApplicationStreamingStore.getStreamerActiveStreamMetadataForStream(stream);
+                    let m4 = ApplicationStreamingStore.getStreamerActiveStreamMetadata({ streamerId: userId });
                     
                     this.sendEvent({
                         type: "debug_log",
-                        message: `announceStreamStart attempt ${attempt} for ${userName}: stream found, meta: ${JSON.stringify(meta)}`
+                        message: `attempt ${attempt} for ${userName}: streamKey: ${streamKey}, m1(userId): ${JSON.stringify(m1)}, m2(streamKey): ${JSON.stringify(m2)}, m3(stream): ${JSON.stringify(m3)}, m4(obj): ${JSON.stringify(m4)}`
                     });
 
+                    let meta = m1 || m2 || m3 || m4;
                     if (meta) {
                         const streamTarget = this.getStreamTargetFromMeta(meta, userId);
                         this.sendEvent({
